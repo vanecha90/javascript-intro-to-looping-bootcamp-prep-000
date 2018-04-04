5f9ef984d2daabf97066e79b8dfd9b2d19e6bcad
vanessa.collado90@gmail.com <vanessa.collado90@gmail.com>
2018-04-04 05:31:42 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -22,13 +22,10 @@ function whileLoop(n){
   return Math.random() >= 0.5
 }
 
-do {
-  console.log('doo-bee-doo-bee-doo')
-} while (maybeTrue());
-
- 
 function doWhileLoop(array){
-  
+  do {
+    array.pop();
+  }
   while (array.length > 0 && maybeTrue());
   return array;
 }
